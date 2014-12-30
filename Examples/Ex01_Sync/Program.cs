e4dc2f2ae24d960f1bb9efcf242c438bad78e4c2
Michael <huanlin.tsai@gmail.com>
2014-12-30 09:33:58 +0800

    Added examples for Chapter 3.

---
+++ Examples/Ex01_Sync/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net;
+
+namespace Ex01_Sync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string content = DownloadPage("http://huan-lin.blogspot.com");
+
+            Console.WriteLine("網頁內容總共為 {0} 個字元。", content.Length);
+            Console.ReadKey();
+        }
+
+        static string DownloadPage(string url)
+        {
+            var webClient = new WebClient();  // 須引用 System.Net 命名空間。
+            string content = webClient.DownloadString(url);
+            return content;
+        }
+    }
+}
---
+++ Examples/Ex02_Async/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ex02_Async
+{
+    class Program
+    {
+        static void ShowThreadInfo(int num, string msg)
+        {
+            Console.WriteLine("({0}) T{1}: {2}", num, Thread.CurrentThread.ManagedThreadId, msg);
+        }
+
+        static void Main(string[] args)
+        {
+            ShowThreadInfo(1, "在 Main() 中， 正要起始非同步工作 DownloadPageAsync()。");
+
+            var task = DownloadPageAsync("http://huan-lin.blogspot.com");
+
+            ShowThreadInfo(3, "在 Main() 中， 已經起始非同步工作 DownloadPageAsync()，但尚未取得工作結果。");
+
+            string content = task.Result;
+
+            ShowThreadInfo(5, "在 Main() 中，已經完成非同步工作 DownloadPageAsync() 並取得結果。");
+
+            Console.WriteLine("網頁內容總共為 {0} 個字元。", content.Length);
+            Console.ReadKey();
+        }
+
+        static async Task<string> DownloadPageAsync(string url)
+        {
+            ShowThreadInfo(2, "在 DownloadPage() 中，正要起始非同步工作 DownloadStringTaskAsync()。");
+
+            var webClient = new WebClient();  // 須引用 System.Net 命名空間。
+            string content = await webClient.DownloadStringTaskAsync(url);
+
+            ShowThreadInfo(4, "在 DownloadPage() 中， 已經完成非同步工作 DownloadStringTaskAsync()。");
+
+            return content;
+        }
+    }
+}
