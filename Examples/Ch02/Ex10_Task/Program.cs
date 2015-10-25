c2ba01de4542f20e8a7fc6bdfcc72e8891ddee8d
Michael Tsai <huanlin.tsai@gmail.com>
2015-10-26 02:38:52 +0800

    added Ex10.

---
+++ Examples/Ch02/Ex10_Task/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ex10_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = new Task(MyTask);
+            task.Start();
+
+            for (int i = 0; i < 500; i++)
+            {
+                Console.Write(".");
+            }
+        }
+
+        static void MyTask()
+        {
+            for (int i = 0; i < 500; i++)
+            {
+                Console.Write("[" + Thread.CurrentThread.ManagedThreadId + "]");
+            }
+        }
+    }
+}
---
+++ Examples/Ch02/Ex10_TaskRun/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ex10_TaskRun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task task = Task.Run(() =>
+                {
+                    for (int i = 0; i < 500; i++)
+                    {
+                        Console.Write("[" + Thread.CurrentThread.ManagedThreadId + "]");
+                    }
+                });
+
+            for (int i = 0; i < 500; i++)
+            {
+                Console.Write(".");
+            }
+
+            task.Wait(); // 確保非同步工作執行完畢之後才往下繼續執行。
+        }
+    }
+}
