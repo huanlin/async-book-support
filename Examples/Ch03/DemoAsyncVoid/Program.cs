667a1ecc49316fbf0de0cb142f96c12aedfde1a7
Michael Tsai <huanlin.tsai@gmail.com>
2015-11-24 01:32:44 +0800

    Minor

---
+++ Examples/Ch03/DemoAsyncVoid/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DemoAsyncVoid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TestVoidAsync();
+            }
+            catch (Exception ex) // 捕捉不到 TestVoidAsync 方法所拋出的異常!
+            {
+                Console.WriteLine(ex);  
+            }
+        }
+
+        static async void TestVoidAsync()
+        {
+            await Task.Delay(1000);
+            throw new Exception("error");
+        }
+    }
+}
