40eae59943d1ac23c298f51c2fae62addf8cebb9
Michael Tsai <huanlin.tsai@gmail.com>
2015-11-25 02:47:07 +0800

    More examples.

---
+++ Examples/Ch04/AsyncLambda/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AsyncLambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 此委派方法需傳入一個 string，且回傳一個 Task<string>。
+            Func<string, Task<string>> downloadPageAsync = 
+                async (string url) =>       // 非同步 Lambda
+                {
+                    using (var client = new HttpClient()) 
+                    {
+                        return await client.GetStringAsync(url);
+                    }
+                };
+
+            var task = downloadPageAsync("http://www.huan-lin.blogspot.com");
+
+            task.Wait(); // 等待工作執行完畢。
+
+            Console.WriteLine("網頁長度: {0}", task.Result.Length);
+        }
+    }
+}
