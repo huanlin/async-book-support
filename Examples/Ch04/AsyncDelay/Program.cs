86b04d89dbf859658dffa0f7177ee04f2e736702
Michael Tsai <huanlin.tsai@gmail.com>
2015-11-25 02:06:53 +0800

    Added Ch04 example.

---
+++ Examples/Ch04/AsyncDelay/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncDelay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task task = Task.Run(() => MyTaskAsync());
+            Console.WriteLine("已經返回 Main()");
+
+            Console.ReadLine();
+        }
+
+        static async Task MyTaskAsync()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(3));
+            Console.WriteLine("非同步工作結束");
+        }
+    }
+}
---
+++ Examples/Ch04/AsyncDelayAndRetry/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AsyncDelayAndRetry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task task = Task.Run(() => MyDownloadPageAsync("xyz"));
+
+            while (task.IsCompleted == false)
+            { } // 等待工作執行完畢
+
+            if (task.IsFaulted)        // 若非同步工作執行失敗
+            {
+                throw task.Exception;  // 便拋出異常
+            }
+            Console.ReadLine();
+        }
+
+        static async Task<string> MyDownloadPageAsync(string url)
+        {
+            const int MaxRetryCount = 3;  // 最多重試 3 次
+
+            using (var client = new HttpClient()) // 先前範例都省略 using，這裡補上。
+            {
+                for (int i = 0; i < MaxRetryCount; i++)
+                {
+                    try
+                    {
+                        return await client.GetStringAsync(url);
+                    }
+                    catch (Exception ex)
+                    {
+                        // 忽略錯誤。
+                        Console.WriteLine("第 {0} 次失敗: {1}", i+1, ex.Message);
+                    }
+                    await Task.Delay(TimeSpan.FromSeconds(i + 1));
+                }
+
+                // 最後一次失敗就讓它拋出異常。
+                return await client.GetStringAsync(url);
+            }
+        }
+    }
+}
