b59f4b81db01cc21e2187bb0dd2c4af2af497edc
Michael Tsai <huanlin.tsai@gmail.com>
2016-01-12 00:22:59 +0800

    Added example of ch04.

---
+++ Examples/Ch04/AsyncWaitAnyTask/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AsyncWaitAllTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(DemoWaitAny);
+            Console.ReadLine();
+        }
+
+        static async Task DemoWaitAny()
+        {
+            Console.WriteLine("DemoWaitAny() 開始");
+
+            var client = new HttpClient();
+
+            Task<string> task1 = client.GetStringAsync("http://microsoft.com");
+            Task<string> task2 = client.GetStringAsync("http://facebook.com");
+            Task<string> task3 = client.GetStringAsync("http://google.com");
+
+            Task<string> completedTask = await Task.WhenAny(task1, task2, task3);
+
+            string content = await completedTask;
+            Console.WriteLine("網頁長度: " + content.Length);
+            Console.WriteLine("DemoWaitAny() 結束");
+        }
+    }
+}
