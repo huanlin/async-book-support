9ede4d5806f6c646ea444731d1343f348082d5e5
Michael Tsai <huanlin.tsai@gmail.com>
2016-01-12 02:01:08 +0800

    more examples.

---
+++ Examples/Ch04/AsyncFactoryMethod/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+
+namespace AsyncFactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Foo
+    {
+        private Foo()   // 把建構函式宣告成 private，不讓外界使用。
+        {
+        }
+
+        // 外界必須透過此方法才能建立物件。
+        public static Task<Foo> CreateInstanceAsync()
+        {
+            var foo = new Foo();          // 建立物件。
+            return foo.InitializeAsync(); // 初始化。
+        }
+
+        private async Task<Foo> InitializeAsync()
+        {
+            //await DomSomeTaskAsync();
+            return this;
+        }
+
+    }
+}
