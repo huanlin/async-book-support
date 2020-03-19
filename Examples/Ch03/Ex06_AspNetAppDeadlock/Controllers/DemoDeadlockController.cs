984a1d3320b5080d5012c744629066ca898ca434
Michael Tsai <huanlin.tsai@gmail.com>
2020-03-19 15:23:39 +0800

    minor

--- Examples/Ch03/Ex06_AspNetAppDeadlock/Controllers/DemoDeadlockController.cs
+++ Examples/Ch03/Ex06_AspNetAppDeadlock/Controllers/DemoDeadlockController.cs
@@ -11,20 +11,20 @@ namespace Ex06_AspNetAppDeadlock.Controllers
         {
             var task = MyDownloadPageAsync("https://www.huanlintalk.com");
             var content = task.Result;
-            return Request.CreateResponse(string.Format("網頁內容總共為 {0} 個字元。", content.Length));
+            return Request.CreateResponse($"網頁內容總共為 {content.Length} 個字元。");
         }
 
         static HttpClient _httpClient = new HttpClient();
 
         private async Task<string> MyDownloadPageAsync(string url)
         {
-            string content = await _httpClient.GetStringAsync(url); // 這裡會 deadlock!
+            string content = await _httpClient.GetStringAsync(url); // deadlock!
             return content;
 
             // return System.Threading.Thread.CurrentThread.ManagedThreadId.ToString();
         }
 
-        // 解法一：使用 Task.ConfigureAwait(false)。此解法有副作用，不是好方法。
+        // 解法一：使用 Task.ConfigureAwait(false)。此解法有副作用，不是頂好的解法。
         // 解法二：從頭到尾都使用 async 方法。這是正解。
     }
 }
