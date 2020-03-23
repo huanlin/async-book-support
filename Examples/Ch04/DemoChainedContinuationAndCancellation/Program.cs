712e33e5ef99f6148ae8c461843ffc6698c9f050
Michael Tsai <huanlin.tsai@gmail.com>
2020-03-23 11:47:53 +0800

    More examples.

---
+++ Examples/Ch04/DemoChainedContinuationAndCancellation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DemoChainedContinuationAndCancellation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task taskA = Task.Run(DoSomething);
+            var cancelToken = new CancellationTokenSource();
+            Task taskB = taskA.ContinueWith(
+                _ => Console.WriteLine("這裡不會執行"), cancelToken.Token);
+            Task taskC = taskB.ContinueWith(
+                _ => Console.WriteLine("TaskC 執行完畢。"));
+            cancelToken.Cancel(); // 取消 taskB
+
+            Console.ReadKey();
+        }
+
+        static void DoSomething()
+        {
+            Thread.Sleep(1500); 
+            Console.WriteLine("TaskA 執行完畢。");
+        }
+    }
+}
