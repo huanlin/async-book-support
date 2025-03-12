d56eb8a729608f38480b98781d8c4b3a6e0e6d55
Michael Tsai <huanlin.tsai@gmail.com>
2025-03-12 23:11:03 +0800

    -

---
+++ Examples/Ch02/LambdaDemo1/Program.cs
@@ -0,0 +1,16 @@
+﻿var demo = new LambdaDemo1();
+demo.InitTimer();
+Console.ReadLine();
+
+public class LambdaDemo1
+{
+    private System.Timers.Timer? _timer;
+    public void InitTimer()
+    {
+        _timer = new System.Timers.Timer(1000);
+        _timer.Elapsed += (sender, args) => Console.WriteLine("X");
+        _timer.Enabled = true;
+    }
+}
+
+
